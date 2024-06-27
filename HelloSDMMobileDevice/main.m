1abb0a47b9dd73b08cc34c866aa1423ab78fc6d9
Danil Korotenko <danil.korotenko@gmail.com>
2024-06-27 14:41:45 +0300

    Hello test program added.

---
+++ HelloSDMMobileDevice/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HelloSDMMobileDevice
+//
+//  Created by Danil Korotenko on 6/27/24.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
